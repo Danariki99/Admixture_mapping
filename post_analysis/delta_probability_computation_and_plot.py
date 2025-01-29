a5a2ca984fa9b6a7a80e46b5891bb343b2dec640
Danariki99 <riccardo.smeriglio@polito.it>
2025-01-29 04:28:20 -0800

    added a saving of all the importand dataframe to have info in case of needs

--- post_analysis/delta_probability_computation_and_plot.py
+++ post_analysis/delta_probability_computation_and_plot.py
@@ -7,6 +7,7 @@ hit_folder_name = '/private/groups/ioannidislab/smeriglio/out_cleaned_codes/vcf_
 dataset_folder = '/private/groups/ioannidislab/smeriglio/out_cleaned_codes/vcf_files_windows/ukbb/probabilities_pipeline/samples'
 models_folder = '/private/groups/ioannidislab/smeriglio/out_cleaned_codes/vcf_files_windows/ukbb/probabilities_pipeline/models'
 plots_folder = '/private/groups/ioannidislab/smeriglio/out_cleaned_codes/vcf_files_windows/ukbb/probabilities_pipeline/plots'
+output_folder = '/private/groups/ioannidislab/smeriglio/out_cleaned_codes/vcf_files_windows/ukbb/probabilities_pipeline/results'
 
 ancestry_list = ['AFR', 'EAS', 'EUR', 'SAS', 'WAS', 'NAT']
 
@@ -26,52 +27,56 @@ for hit in hits_list:
         current_model_folder = f'{models_folder}/{hit}/{ancestry}'
         current_dataset_folder = f'{dataset_folder}/{hit}/{ancestry}'
 
+        result_folder = os.path.join(output_folder, hit, ancestry)
+        os.makedirs(result_folder, exist_ok=True)
+
         snps_list = os.listdir(current_model_folder)
         snps_list = [file_name.replace('.csv', '') for file_name in snps_list if file_name.endswith('.csv')]
         if len(snps_list) != 0:
 
             filtered_df = df[(df['TEST'] == imp_ancestry) & (df['ID'].isin(snps_list))]
-            most_significant_SNP = filtered_df.loc[filtered_df['P'].idxmin()]
+            most_significant_SNP = filtered_df.loc[filtered_df['P'].idxmin()]['ID']
+            for snp in snps_list:
+                dataset = pd.read_csv(f'{current_dataset_folder}/{snp}.tsv', sep='\t')
+                dataset = dataset[dataset[imp_ancestry] == 1]
 
-            dataset = pd.read_csv(f'{current_dataset_folder}/{most_significant_SNP["ID"]}.tsv', sep='\t')
-            dataset = dataset[dataset[imp_ancestry] == 1]
-            
+                model = pd.read_csv(f'{current_model_folder}/{snp}.csv')
 
-            model = pd.read_csv(f'{current_model_folder}/{most_significant_SNP["ID"]}.csv')
+                no_standardize_covars = ['IID', 'sex', imp_ancestry, 'ADD']
 
-            no_standardize_covars = ['IID', 'sex', imp_ancestry, 'ADD']
+                columns_to_standardize = [col for col in dataset.columns if col not in no_standardize_covars]
 
-            columns_to_standardize = [col for col in dataset.columns if col not in no_standardize_covars]
+                dataset[columns_to_standardize] = (dataset[columns_to_standardize] - dataset[columns_to_standardize].mean()) / dataset[columns_to_standardize].std()
 
-            dataset[columns_to_standardize] = (dataset[columns_to_standardize] - dataset[columns_to_standardize].mean()) / dataset[columns_to_standardize].std()
+                interesting_columns = model.columns.tolist()[3:]
 
-            interesting_columns = model.columns.tolist()[3:]
+                intercept = model['INTERCEPT']
 
-            intercept = model['INTERCEPT']
+                # Applica i coefficienti e l'intercetta a ogni riga del dataset
+                dataset['z_with'] = intercept.values[0]  # Inizia con l'intercetta
+                dataset['z_without'] = intercept.values[0]  # Inizia con l'intercetta
 
-            # Applica i coefficienti e l'intercetta a ogni riga del dataset
-            dataset['z_with'] = intercept.values[0]  # Inizia con l'intercetta
-            dataset['z_without'] = intercept.values[0]  # Inizia con l'intercetta
+                for column in interesting_columns:
+                    if column in dataset.columns:
+                        if column == imp_ancestry:
+                            dataset['z_without'] += dataset[column] * model[column].values[0]  # Escludi imp_ancestry
+                        dataset['z_with'] += dataset[column] * model[column].values[0]  # Aggiungi il contributo
 
-            for column in interesting_columns:
-                if column in dataset.columns:
-                    if column == imp_ancestry:
-                        dataset['z_without'] += dataset[column] * model[column].values[0]  # Escludi imp_ancestry
-                    dataset['z_with'] += dataset[column] * model[column].values[0]  # Aggiungi il contributo
+                # Calcola le probabilità e la differenza
+                dataset['P_with'] = expit(dataset['z_with'])
+                dataset['P_without'] = expit(dataset['z_without'])
+                dataset['delta_P'] = dataset['P_with'] - dataset['P_without']
+                dataset['delta_P_abs'] = abs(dataset['P_with'] - dataset['P_without'])
 
-            # Calcola le probabilità e la differenza
-            dataset['P_with'] = expit(dataset['z_with'])
-            dataset['P_without'] = expit(dataset['z_without'])
-            dataset['delta_P'] = dataset['P_with'] - dataset['P_without']
-            dataset['delta_P_abs'] = abs(dataset['P_with'] - dataset['P_without'])
+                df.to_csv(os.path.join(result_folder, f'{snp}.tsv'), sep='\t', index=False)
 
-            deltas.append(dataset['delta_P'].tolist())
-            abs_deltas.append(dataset['delta_P_abs'].tolist())
+                if snp == most_significant_SNP:
+                    deltas.append(dataset['delta_P'].tolist())
+                    abs_deltas.append(dataset['delta_P_abs'].tolist())
 
-        else:
-
-            deltas.append([])
-            abs_deltas.append([])
+            else:
+                deltas.append([])
+                abs_deltas.append([])
         
     # let's make the plot at the end of the iteration for all the ancestries
     plots_folder = os.path.join(plots_folder, hit)
